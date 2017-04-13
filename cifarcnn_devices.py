d1a8730cf3362993b0e75e3d53aa6c6ef51cb0ae
Kurt Portelli <kurtportelli@gmail.com>
2017-04-13 15:34:58 +0100

    restructuring devices code

--- cifarcnn_devices.py
+++ cifarcnn_devices.py
@@ -3,62 +3,25 @@ import numpy as np
 import time
 from datetime import timedelta
 import os
-
-# Use PrettyTensor to simplify Neural Network construction.
 import prettytensor as pt
 import cifar10s as cifar10
 from cifar10s import img_size, num_channels, num_classes
 import helper
 
-cifar10.maybe_download_and_extract()
-class_names = cifar10.load_class_names()
-print(class_names)
+save_dir = 'checkpoints/'
+train_sum_dir = 'checkpoints/train_summaries/'
 
-# 50,000 images/labels
-images_train, cls_train, labels_train = cifar10.load_training_data()
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
 
-# 10,000 images/labels
-images_test, cls_test, labels_test = cifar10.load_test_data()
-
-print("Size of:")
-print("- Training-set:\t\t{}".format(len(images_train)))
-print("- Test-set:\t\t{}".format(len(images_test)))
-
-# split into a list per label
-images_train_group = [[] for n in range(10)]
-cls_train_group = [[] for n in range(10)]
-labels_train_group = [[] for n in range(10)]
-images_test_group = [[] for n in range(10)]
-cls_test_group = [[] for n in range(10)]
-labels_test_group = [[] for n in range(10)]
-
-for i in range(50000):
-    cur_label = cls_train[i]
-    images_train_group[cur_label].append(images_train[i])
-    labels_train_group[cur_label].append(labels_train[i])
-    cls_train_group[cur_label].append(cls_train[i])
-
-for i in range(10000):
-    cur_label = cls_test[i]
-    images_test_group[cur_label].append(images_test[i])
-    labels_test_group[cur_label].append(labels_test[i])
-    cls_test_group[cur_label].append(cls_test[i])
-
+save_path = os.path.join(save_dir, 'cifar10_cnn')
+train_batch_size = 64
 img_size_cropped = 24
 clients = 100
 training_per_client = 500
 testing_per_client = 100
-
-# split into a list per device
-images_train_device = [[] for n in range(clients)]
-cls_train_device = [[] for n in range(clients)]
-labels_train_device = [[] for n in range(clients)]
-images_test_device = [[] for n in range(clients)]
-cls_test_device = [[] for n in range(clients)]
-labels_test_device = [[] for n in range(clients)]
-
-trainFile = 'train.txt'
-testFile = 'test.txt'
+train_file = 'train.txt'
+test_file = 'test.txt'
 
 
 def update_device_data(file, i_t_d, c_t_d, l_t_d, i_t_g, c_t_g, l_t_g):
@@ -75,14 +38,6 @@ def update_device_data(file, i_t_d, c_t_d, l_t_d, i_t_g, c_t_g, l_t_g):
                 l_t_d[d].append(l_t_g[lbls_ids[i]][imgs_ids[i]])
 
             d += 1
-
-
-update_device_data(trainFile,
-                   images_train_device, cls_train_device, labels_train_device,
-                   images_train_group, cls_train_group, labels_train_group)
-update_device_data(testFile,
-                   images_test_device, cls_test_device, labels_test_device,
-                   images_test_group, cls_test_group, labels_test_group)
 
 
 def pre_process_image(image, training):
@@ -130,7 +85,7 @@ def pre_process(images, training):
     return images
 
 
-def main_network(images, training):
+def main_network(images, training, y_true):
     # Wrap the input images as a Pretty Tensor object.
     x_pretty = pt.wrap(images)
 
@@ -158,23 +113,20 @@ def main_network(images, training):
     return y_pred, loss
 
 
-def create_network(training):
+def create_network(training, input_image, y_true):
     # Wrap the neural network in the scope named 'network'.
     # Create new variables during training, and re-use during testing.
     with tf.variable_scope('network', reuse=not training):
-        # Just rename the input placeholder variable for convenience.
-        images = x
-
         # Create TensorFlow graph for pre-processing.
-        images = pre_process(images=images, training=training)
+        images = pre_process(images=input_image, training=training)
 
         # Create TensorFlow graph for the main processing.
-        y_pred, loss = main_network(images=images, training=training)
+        y_pred, loss = main_network(images=images, training=training, y_true=y_true)
 
     return y_pred, loss
 
 
-def random_batch():
+def random_batch(images_train, labels_train):
     # Number of images in the training-set.
     num_images = len(images_train)
 
@@ -190,7 +142,7 @@ def random_batch():
     return x_batch, y_batch
 
 
-def optimize(num_iterations):
+def optimize(num_iterations, images_train, labels_train, session, saver, merged, train_writer, global_step, accuracy, optimizer, x, y_true):
     # Start-time used for printing time-usage below.
     start_time = time.time()
 
@@ -198,7 +150,7 @@ def optimize(num_iterations):
         # Get a batch of training examples.
         # x_batch now holds a batch of images and
         # y_true_batch are the true labels for those images.
-        x_batch, y_true_batch = random_batch()
+        x_batch, y_true_batch = random_batch(images_train, labels_train)
 
         # Put the batch into a dict with the proper names
         # for placeholder variables in the TensorFlow graph.
@@ -243,79 +195,118 @@ def optimize(num_iterations):
     print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
 
 
-# Get the first images from the test-set.
-# images = images_test[0:9]
-# Get the true classes for those images.
-# cls_true = cls_test[0:9]
-# Plot the images and labels using our helper-function above.
-# plot_images(images=images, cls_true=cls_true, smooth=True)
+def main():
 
-x = tf.placeholder(tf.float32, shape=[None, img_size, img_size, num_channels], name='x')
-y_true = tf.placeholder(tf.float32, shape=[None, num_classes], name='y_true')
+    cifar10.maybe_download_and_extract()
+    class_names = cifar10.load_class_names()
+    print(class_names)
 
-y_true_cls = tf.argmax(y_true, dimension=1)
+    # 50,000 images/labels
+    images_train, cls_train, labels_train = cifar10.load_training_data()
 
-distorted_images = pre_process(images=x, training=True)
+    # 10,000 images/labels
+    images_test, cls_test, labels_test = cifar10.load_test_data()
 
-# Create Neural Network for Training Phase
-global_step = tf.Variable(initial_value=0, name='global_step', trainable=False)
+    print("Size of:")
+    print("- Training-set:\t\t{}".format(len(images_train)))
+    print("- Test-set:\t\t{}".format(len(images_test)))
 
-_, loss = create_network(training=True)
+    # split into a list per label
+    images_train_group = [[] for n in range(10)]
+    cls_train_group = [[] for n in range(10)]
+    labels_train_group = [[] for n in range(10)]
+    images_test_group = [[] for n in range(10)]
+    cls_test_group = [[] for n in range(10)]
+    labels_test_group = [[] for n in range(10)]
 
-optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss, global_step=global_step)
+    for i in range(50000):
+        cur_label = cls_train[i]
+        images_train_group[cur_label].append(images_train[i])
+        labels_train_group[cur_label].append(labels_train[i])
+        cls_train_group[cur_label].append(cls_train[i])
 
-# Create Neural Network for Test Phase / Inference
-y_pred, _ = create_network(training=False)
+    for i in range(10000):
+        cur_label = cls_test[i]
+        images_test_group[cur_label].append(images_test[i])
+        labels_test_group[cur_label].append(labels_test[i])
+        cls_test_group[cur_label].append(cls_test[i])
 
-y_pred_cls = tf.argmax(y_pred, dimension=1)
+    # split into a list per device
+    images_train_device = [[] for n in range(clients)]
+    cls_train_device = [[] for n in range(clients)]
+    labels_train_device = [[] for n in range(clients)]
+    images_test_device = [[] for n in range(clients)]
+    cls_test_device = [[] for n in range(clients)]
+    labels_test_device = [[] for n in range(clients)]
 
-correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+    update_device_data(train_file,
+                       images_train_device, cls_train_device, labels_train_device,
+                       images_train_group, cls_train_group, labels_train_group)
+    update_device_data(test_file,
+                       images_test_device, cls_test_device, labels_test_device,
+                       images_test_group, cls_test_group, labels_test_group)
 
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    # prepare input placeholders in tensorflow
+    x = tf.placeholder(tf.float32, shape=[None, img_size, img_size, num_channels], name='x')
+    y_true = tf.placeholder(tf.float32, shape=[None, num_classes], name='y_true')
 
-saver = tf.train.Saver()
+    y_true_cls = tf.argmax(y_true, dimension=1)
 
-tf.summary.scalar('accuracy', accuracy)
+    distorted_images = pre_process(images=x, training=True)
 
-# weights_conv1 = helper.get_weights_variable(layer_name='layer_conv1')
-# weights_conv2 = helper.get_weights_variable(layer_name='layer_conv2')
-# output_conv1 = helper.get_layer_output(layer_name='layer_conv1')
-# output_conv2 = helper.get_layer_output(layer_name='layer_conv2')
+    # Create Neural Network for Training Phase
+    global_step = tf.Variable(initial_value=0, name='global_step', trainable=False)
 
-session = tf.Session()
+    _, loss = create_network(training=True, input_image=x, y_true=y_true)
 
-save_dir = 'checkpoints/'
-train_sum_dir = 'checkpoints/train_summaries/'
+    optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss, global_step=global_step)
 
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
+    # Create Neural Network for Test Phase / Inference
+    y_pred, _ = create_network(training=False, input_image=x, y_true=y_true)
 
-save_path = os.path.join(save_dir, 'cifar10_cnn')
+    y_pred_cls = tf.argmax(y_pred, dimension=1)
 
-try:
-    print("Trying to restore last checkpoint ...")
+    correct_prediction = tf.equal(y_pred_cls, y_true_cls)
 
-    # Use TensorFlow to find the latest checkpoint - if any.
-    last_chk_path = tf.train.latest_checkpoint(checkpoint_dir=save_dir)
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    # Try and load the data in the checkpoint.
-    saver.restore(session, save_path=last_chk_path)
+    saver = tf.train.Saver()
 
-    # If we get to this point, the checkpoint was successfully loaded.
-    print("Restored checkpoint from:", last_chk_path)
-except:
-    # If the above failed for some reason, simply
-    # initialize all the variables for the TensorFlow graph.
-    print("Failed to restore checkpoint. Initializing variables instead.")
-    session.run(tf.global_variables_initializer())
+    tf.summary.scalar('accuracy', accuracy)
 
-train_batch_size = 64
+    # weights_conv1 = helper.get_weights_variable(layer_name='layer_conv1')
+    # weights_conv2 = helper.get_weights_variable(layer_name='layer_conv2')
+    # output_conv1 = helper.get_layer_output(layer_name='layer_conv1')
+    # output_conv2 = helper.get_layer_output(layer_name='layer_conv2')
 
-merged = tf.summary.merge_all()
-train_writer = tf.summary.FileWriter(train_sum_dir, session.graph)
+    session = tf.Session()
 
-optimize(num_iterations=100000)
+    try:
+        print("Trying to restore last checkpoint ...")
 
-helper.print_test_accuracy(session, images_test, labels_test, cls_test,
-                           x, y_true, y_pred_cls, num_classes, class_names,
-                           show_example_errors=True, show_confusion_matrix=True)
+        # Use TensorFlow to find the latest checkpoint - if any.
+        last_chk_path = tf.train.latest_checkpoint(checkpoint_dir=save_dir)
+
+        # Try and load the data in the checkpoint.
+        saver.restore(session, save_path=last_chk_path)
+
+        # If we get to this point, the checkpoint was successfully loaded.
+        print("Restored checkpoint from:", last_chk_path)
+    except:
+        # If the above failed for some reason, simply
+        # initialize all the variables for the TensorFlow graph.
+        print("Failed to restore checkpoint. Initializing variables instead.")
+        session.run(tf.global_variables_initializer())
+
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter(train_sum_dir, session.graph)
+
+    optimize(1000, images_train, labels_train, session, saver, merged, train_writer, global_step, accuracy, optimizer, x, y_true)
+
+    helper.print_test_accuracy(session, images_test, labels_test, cls_test,
+                               x, y_true, y_pred_cls, num_classes, class_names,
+                               show_example_errors=True, show_confusion_matrix=True)
+
+
+if __name__ == "__main__":
+    main()
